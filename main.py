c1a2a5bb10f0e59b7a3441843fc29cda360ad93d
Rob <robertmadill17@gmail.com>
2022-02-11 23:01:24 -0500

    main, requirements and resources added
    First initial commit of main and requirements files. also committing resources folder to hold stories

---
+++ main.py
@@ -0,0 +1,4 @@
+# video reference: https://www.youtube.com/watch?v=dvAurfBB6Jk
+import tweepy
+# request from developer.twitter.com -> apps (could take few days or weeks)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
