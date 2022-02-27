5e4a98b0035b251883c3b3a8b7a68e8973134c94
Rob <robertmadill17@gmail.com>
2022-02-26 20:12:33 -0500

    Updating project structure - old twitter auth
    original twitter authentication - will change to bearer token for easier access

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-# video reference: https://www.youtube.com/watch?v=dvAurfBB6Jk
+# tweepy - video reference: https://www.youtube.com/watch?v=dvAurfBB6Jk
+# tweepy - documentation: https://docs.tweepy.org/en/stable/authentication.html#
 import tweepy
-# request from developer.twitter.com -> apps (could take few days or weeks)
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+import argparse
+import config
+
+
+def main():
+    # Change to OAuth Bearer token (App Only)
+    consumer_key = config.API_KEY
+    consumer_secret = config.API_KEY_SECRET
+    access_token = config.ACCESS_TOKEN
+    access_token_secret = config.ACCESS_TOKEN_SECRET
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    user = api.get_user()
+
+    return print(user.screen_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="reads through content and tweets one sentence at a time")
+    parser.add_argument('-d', '--data-file', dest='data_file',
+                        metavar='', help='content that will be parsed and tweeted')
+    args = parser.parse_args()
+    data_file = args.data_file
+    main()
