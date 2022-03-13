5f5c0f20afa6eb2e293d53e2fed593759af22d07
Rob <robertmadill17@gmail.com>
2022-03-12 22:04:05 -0500

    created authentication function
    created authentication function and started to read through content

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-# tweepy - video reference: https://www.youtube.com/watch?v=dvAurfBB6Jk
+# tweepy - guide: https://realpython.com/twitter-bot-python-tweepy/
 # tweepy - documentation: https://docs.tweepy.org/en/stable/authentication.html#
 import tweepy
 import argparse
 import config
 
 
+def auth() -> object:
+    auth = tweepy.OAuthHandler(config.API_KEY, config.API_KEY_SECRET)
+    auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
+
+
 def main():
-    # Change to OAuth Bearer token (App Only)
-    consumer_key = config.API_KEY
-    consumer_secret = config.API_KEY_SECRET
-    access_token = config.ACCESS_TOKEN
-    access_token_secret = config.ACCESS_TOKEN_SECRET
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    user = api.get_user()
-
-    return print(user.screen_name)
+    api = auth()
+    content = 'resources\The_Idiot_Part_One.txt'
+    with open(content, mode='r', newline='') as c:
+        return 0
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="reads through content and tweets one sentence at a time")
-    parser.add_argument('-d', '--data-file', dest='data_file',
+    parser.add_argument('-c', '--content', dest='content',
                         metavar='', help='content that will be parsed and tweeted')
     args = parser.parse_args()
-    data_file = args.data_file
+    content = args.content
     main()
