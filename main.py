fc0293999314667c0d1c5415e63d6c678462ed42
Rob <robertmadill17@gmail.com>
2022-03-17 19:07:28 -0400

    Added cmd line arguments to pass to main function

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 # tweepy - guide (for authentication): https://realpython.com/twitter-bot-python-tweepy/
-# tweepy - documentation: https://docs.tweepy.org/en/stable/authentication.html#
+# tweepy - full documentation: https://docs.tweepy.org/en/stable/getting_started.html
+# tweepy - API reference: https://docs.tweepy.org/en/stable/api.html#api-reference
 import tweepy
 import argparse
 import config
 
-
-def auth() -> object:
+# Passes twitter authentication   
+def auth() -> API:
     auth = tweepy.OAuthHandler(config.API_KEY, config.API_KEY_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
     return tweepy.API(auth)
 
-
-def main():
+# Reads content with keyword selection and sends as tweet
+def main(content: str, keyword: str) -> None:
     api = auth()
-    content = 'resources\The_Idiot_Part_One.txt'
-    keywords = 'Towards'
     with open(content, mode='r', newline='') as c:
-        # Will print line that contains keyword
         for line in c:
-            if keywords in line:
+            if keyword in line:
                 api.update_status(line)
 
-
+# Used for command line arguments when running script
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="reads through content and tweets one sentence at a time")
     parser.add_argument('-c', '--content', dest='content',
-                        metavar='', help='content that will be parsed and tweeted')
+                        metavar='', help='content that will be parsed and tweeted', required=True)
     parser.add_argument('-k', '--keyword', dest='keyword',
-                        metavar='', help='keyword to find in content file')
+                        metavar='', help='keyword to find in content file', required=True)
+
     args = parser.parse_args()
     content = args.content
     keyword = args.keyword
-    main()
+
+    main(content, keyword)
